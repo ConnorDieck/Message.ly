ec71a3f80f14b4cf29372e2b87ea25fcc2df6b04
Connor Dieck <connordieck@gmail.com>
2021-11-01 14:56:58 -0400

    added user model methods

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
+/** Routes for demonstrating authentication in Express. */
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = new express.Router();
+
+const { SECRET_KEY } = require("../config");
+const User = require("../models/user");
+const ExpressError = require("../expressError");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +20,18 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async (req, res, next) => {
+	try {
+		let { username } = await User.register(req.body);
+		let token = jwt.sign({ username }, SECRET_KEY);
+		return res.json({ token });
+	} catch (e) {
+		// if (e.code === "23505") {
+		// 	return next(new ExpressError("Username taken. Please pick another!", 400));
+		// }
+		next(e);
+	}
+});
+
+module.exports = router;
